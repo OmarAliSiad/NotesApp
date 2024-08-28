145b4e6556b77f07ee6908592045d1e154e9ea54
omarali <omaraliabdelaziz250@gmail.com>
2024-08-28 22:20:57 +0300

    dark mode

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+ThemeData DarkTheme(ThemeMode mode) {
+  return ThemeData(
+    appBarTheme: AppBarTheme(
+        titleTextStyle: TextStyle(
+            fontSize: 27,
+            color: (ThemeMode.light == mode) ? Colors.black : Colors.white)),
+    brightness: Brightness.dark,
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+          color: (ThemeMode.light == mode) ? Colors.black : Colors.white),
+      bodyMedium: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: (ThemeMode.light == mode) ? Colors.black : Colors.white),
+      bodySmall: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: (ThemeMode.light == mode) ? Colors.black : Colors.white),
+    ),
+  );
+}
