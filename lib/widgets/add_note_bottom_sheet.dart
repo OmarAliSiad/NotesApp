a5a5b61bc7b7a6c731b6debf9adc05ee740749e6
omarali <omaraliabdelaziz250@gmail.com>
2024-08-30 14:33:45 +0300

    Refactor AddNoteBottomSheet to use StatefulWidget and Form Validation
    Convert AddNoteBottomSheet from a StatelessWidget to a StatefulWidget to manage state and form validation. Introduce a GlobalKey for the form state and implement onSaved callbacks for text fields to capture input values. Update the CustomButton to trigger form validation and save form state, printing captured values for demonstration purposes.

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -3,43 +3,69 @@ import 'package:notesapp/widgets/custom_button.dart';
 import 'package:notesapp/widgets/custom_text_field.dart';
 import 'package:notesapp/widgets/mode.dart';
 
-class AddNoteBottomSheet extends StatelessWidget {
+class AddNoteBottomSheet extends StatefulWidget {
   ValueNotifier<ThemeMode> valueNotifier;
-  ThemeMode mode = ThemeMode.light;
+  static final formState = GlobalKey<FormState>();
+
   AddNoteBottomSheet({super.key, required this.valueNotifier});
+
+  @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
+
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+  ThemeMode mode = ThemeMode.light;
+  String? title, subtitle;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomTextField(
-              hint_color: Colors.blue,
-              mode: mode,
-              maxLines: 1,
-              valueNotifier: valueNotifier,
-              hintText: 'Title',
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            CustomTextField(
-              hint_color: Colors.blue,
-              mode: mode,
-              maxLines: 5,
-              valueNotifier: valueNotifier,
-              hintText: 'Content',
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const CustomButton(),
-            const SizedBox(
-              height: 16,
-            ),
-          ],
+        child: Form(
+          key: AddNoteBottomSheet.formState,
+          child: Column(
+            children: [
+              CustomTextField(
+                onSaved: (value) {
+                  title = value;
+                },
+                hint_color: Colors.blue,
+                mode: mode,
+                maxLines: 1,
+                valueNotifier: widget.valueNotifier,
+                hintText: 'Title',
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextField(
+                onSaved: (value) {
+                  subtitle = value;
+                },
+                hint_color: Colors.blue,
+                mode: mode,
+                maxLines: 5,
+                valueNotifier: widget.valueNotifier,
+                hintText: 'Content',
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              CustomButton(
+                onTap: () {
+                  if (AddNoteBottomSheet.formState.currentState!.validate()) {
+                    AddNoteBottomSheet.formState.currentState!.save();
+                    print(title);
+                    print(subtitle);
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
         ),
       ),
     );
