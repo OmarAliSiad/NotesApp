11494d71f086205ae33b5ea11912e1fe0491b4b4
omarali <omaraliabdelaziz250@gmail.com>
2024-08-30 14:21:32 +0300

    add GestureDetector

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, required this.onTap});
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          color: Colors.blue[800], borderRadius: BorderRadius.circular(8)),
-      child: const Center(child: Text('Add')),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.blue[800], borderRadius: BorderRadius.circular(8)),
+        child: const Center(child: Text('Add')),
+      ),
     );
   }
 }
