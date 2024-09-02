9d7492b11cd196a77deb2429b4b472b164d804de
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 22:55:11 +0300

    edit note view that take note model and pass it to cubit

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/model/note_model.dart';
 import 'package:notesapp/widgets/edit_note_view_body.dart';
 
 class EditNoteView extends StatelessWidget {
   static String id = 'EditNoteView';
-  const EditNoteView({super.key});
+  const EditNoteView({super.key, required this.noteModel});
+  final NoteModel noteModel;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: EditNoteViewBody(),
+    return Scaffold(
+      body: EditNoteViewBody(
+        noteModel: noteModel,
+      ),
     );
   }
 }
