af43d357646e0b18e2519e369992fb3f8184d08e
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 23:05:38 +0300

    triger BlocProvider of type AddNote cubit to store model after check the user enter the title and content

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notesapp/model/note_model.dart';
+import 'package:notesapp/theme/dark_theme.dart';
 import 'package:notesapp/widgets/custom_app_bar.dart';
 import 'package:notesapp/widgets/custom_text_field.dart';
+import 'package:notesapp/widgets/list_view_colors%20_item.dart';
 
 class EditNoteViewBody extends StatefulWidget {
-  const EditNoteViewBody({super.key});
+  const EditNoteViewBody({super.key, required this.noteModel});
+  final NoteModel noteModel;
 
   @override
   State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
@@ -14,8 +20,20 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CustomAppBar(title: 'Edits Note', icon: Icons.check),
+        CustomAppBar(
+            title: 'Edits Note',
+            icon: Icons.check,
+            ontap: () {
+              widget.noteModel.title = title ?? widget.noteModel.title;
+              widget.noteModel.description =
+                  subtitle ?? widget.noteModel.description;
+              widget.noteModel.color =
+                  BlocProvider.of<AddNoteCubit>(context).color.value;
+              widget.noteModel.save();
+              Navigator.of(context).pop();
+            }),
         const SizedBox(
           height: 20,
         ),
@@ -24,27 +42,42 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
           child: Column(
             children: [
               CustomTextField(
-                onSaved: (value) {
+                onChanged: (value) {
                   title = value;
                 },
                 hint_color: Colors.grey,
                 maxLines: 1,
-                hintText: 'Title',
+                hintText: widget.noteModel.title,
               ),
               const SizedBox(
                 height: 15,
               ),
               CustomTextField(
-                onSaved: (value) {
+                onChanged: (value) {
                   subtitle = value;
                 },
                 hint_color: Colors.grey,
                 maxLines: 5,
-                hintText: 'Content',
+                hintText: widget.noteModel.description,
               ),
             ],
           ),
-        )
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Text(
+            'Choose Color',
+            style:
+                TextStyle(fontSize: darkTheme().textTheme.bodyLarge!.fontSize),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        const ColorListView(),
       ],
     );
   }
