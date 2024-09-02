f3d46e422a71a2c8251349ab8eaede172b035e76
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 16:05:45 +0300

    Refactor AddNoteBottomSheet to improve state management and error handling
    - Removed unused imports and dependencies.
    - Simplified the widget structure by removing unnecessary layers and widgets.
    - Integrated BlocProvider to manage the AddNoteCubit state more effectively.
    - Enhanced error handling by printing error messages directly to the console instead of showing SnackBars.
    - Added automatic fetching of all notes after successfully adding a new note to keep the UI in sync.
    - Improved responsiveness by using AbsorbPointer to disable interactions during loading states.

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notesapp/cubits/note_cubit/note_cubit.dart';
 import 'package:notesapp/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
-  ValueNotifier<ThemeMode> valueNotifier;
   static final formState = GlobalKey<FormState>();
-  AddNoteBottomSheet({super.key, required this.valueNotifier});
+  const AddNoteBottomSheet({super.key});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: SingleChildScrollView(
-          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
         listener: (context, state) {
           if (state is AddNoteSuccess) {
+            BlocProvider.of<NotesCubit>(context).fetchAllNotes();
             Navigator.pop(context);
+            print('added\n');
           } else if (state is AddNoteFaiulre) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text(state.errorMessage),
-            ));
+            print('${state.errorMessage}\n');
           }
         },
         builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNoteLoading ? true : false,
-            child: AddNoteForm(
-              valueNotifier: valueNotifier,
-            ),
+          return BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return AbsorbPointer(
+                absorbing: state is AddNoteLoading ? true : false,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0,
+                      left: 16,
+                      bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: const SingleChildScrollView(
+                    child: AddNoteForm(),
+                  ),
+                ),
+              );
+            },
           );
         },
-      )),
+      ),
     );
   }
 }
