1563ca7218baacb0086cd53d6b833c029b994442
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 15:52:24 +0300

    Refactor main.dart to integrate NotesCubit and update UI components

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,37 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notesapp/costants/costants.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notesapp/cubits/note_cubit/note_cubit.dart';
 import 'package:notesapp/model/note_model.dart';
 import 'package:notesapp/simple_bloc_observer.dart';
 import 'package:notesapp/theme/dark_theme.dart';
-import 'package:notesapp/theme/light%20_theme.dart';
 import 'package:notesapp/views/edit_note_view.dart';
-import 'package:notesapp/views/home_page.dart';
+import 'package:notesapp/views/NoteView.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kbox);
   Bloc.observer = SimpleBlocObserver();
-  runApp(NotesApp());
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
-  final ValueNotifier<ThemeMode> valueNotifier = ValueNotifier(ThemeMode.light);
-  NotesApp({super.key});
+  const NotesApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-        valueListenable: valueNotifier,
-        builder: (context, mode, widget) {
-          return MaterialApp(
-              routes: {
-                EditNoteView.id: (context) => const EditNoteView(),
-              },
-              themeMode: mode,
-              theme: LightTheme(mode),
-              darkTheme: DarkTheme(mode),
-              debugShowCheckedModeBanner: false,
-              home: HomePage(
-                valueNotifier: valueNotifier,
-              ));
-        });
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: MaterialApp(
+          routes: {
+            EditNoteView.id: (context) => const EditNoteView(),
+          },
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            fontFamily: 'Roboto',
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const NoteView()),
+    );
   }
 }
