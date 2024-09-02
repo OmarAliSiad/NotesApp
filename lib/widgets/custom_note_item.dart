fab81db2af2d4d6b14bf2d73a5d27668f18587d5
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 16:07:53 +0300

    Update CustomNoteItem Widget to Use NoteModel and Integrate with Cubit
    The CustomNoteItem widget has been updated to utilize the NoteModel for displaying note details and to integrate with the NotesCubit for managing note state. This includes:
    
    - Replacing the generic color parameter with a NoteModel instance to display specific note details.
    - Implementing a delete functionality that uses the NoteModel's delete method and refreshes the note list via the NotesCubit.
    - Adjusting the UI to display note title, description, and date directly from the NoteModel.
    - Wrapping the delete icon with an IconButton to enable the delete action on press.
    - Importing and using flutter_bloc for state management and note_cubit for handling note operations.
    
    This update enhances the widget's functionality and ensures it properly interacts with the application's state management system.

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notesapp/cubits/note_cubit/note_cubit.dart';
+import 'package:notesapp/model/note_model.dart';
 import 'package:notesapp/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
-  final Color color;
-  const CustomNoteItem({super.key, required this.color});
+  final NoteModel note;
+  const CustomNoteItem({super.key, required this.note});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(context, EditNoteView.id); 
+        Navigator.pushNamed(context, EditNoteView.id);
       },
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        margin: const EdgeInsets.only(bottom: 15),
+        margin: const EdgeInsets.only(bottom: 0),
         decoration: BoxDecoration(
-            color: color, borderRadius: BorderRadius.circular(16)),
+            color: Color(note.color), borderRadius: BorderRadius.circular(16)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
@@ -26,27 +29,36 @@ class CustomNoteItem extends StatelessWidget {
               title: Padding(
                 padding: const EdgeInsets.only(bottom: 20),
                 child: Text(
-                  'Flutter tips',
+                  note.title,
                   style: TextStyle(
                       fontSize: 28, color: Colors.black.withOpacity(.9)),
                 ),
               ),
               subtitle: Text(
-                'Build Your Career with Thrwat Samy',
+                note.description,
                 style: TextStyle(
                     fontSize: 20, color: Colors.black.withOpacity(.5)),
               ),
-              trailing: const Icon(
-                FontAwesomeIcons.trash,
+              trailing: IconButton(
+                onPressed: () {
+                  note.delete();
+                  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                },
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  size: 25,
+                ),
                 color: Colors.black,
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(right: 12, bottom: 10, top: 10),
               child: Text(
-                'May 21.2022',
+                note.date,
                 style: TextStyle(
-                    fontSize: 16, color: Colors.black.withOpacity(.5)),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(.5)),
               ),
             ),
           ],
