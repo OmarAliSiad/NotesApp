6c98b815d5ee6a781d0830c2c427a935fc9c89c8
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 16:07:20 +0300

    add custom button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.onTap});
+  const CustomButton({super.key, required this.onTap, this.isloading = false});
   final void Function()? onTap;
+  final bool isloading;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,17 @@ class CustomButton extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
             color: Colors.blue[800], borderRadius: BorderRadius.circular(8)),
-        child: const Center(child: Text('Add')),
+        child: (isloading)
+            ? const Center(
+                child: SizedBox(
+                height: 30,
+                width: 30,
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                  backgroundColor: Colors.white,
+                ),
+              ))
+            : const Center(child: Text('Add')),
       ),
     );
   }
