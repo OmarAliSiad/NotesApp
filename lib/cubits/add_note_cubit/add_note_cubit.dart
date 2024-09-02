7495feb2f241d32ebbfd2792ccd1dc79da8491cd
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 23:07:26 +0300

    add note cubit add method that make sure the note is added

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notesapp/costants/costants.dart';
 import 'package:notesapp/model/note_model.dart';
@@ -7,14 +8,13 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color color = Colors.white;
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       Box<NoteModel> notesBox = Hive.box<NoteModel>(kbox);
       await notesBox.add(note);
-      print(
-          'notes Box : ${notesBox.values.first}\n notesBox : ${notesBox.isEmpty} ');
       emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNoteFaiulre(errorMessage: e.toString()));
