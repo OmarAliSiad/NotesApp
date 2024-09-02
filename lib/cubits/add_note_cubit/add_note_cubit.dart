ee48d6cbbd3e56c421de101aec0db27a5dc5e43e
omarali <omaraliabdelaziz250@gmail.com>
2024-09-02 15:52:59 +0300

    Remove unnecessary import and fix synchronous note addition in AddNoteCubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:bloc/bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notesapp/costants/costants.dart';
@@ -10,11 +8,13 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(NoteModel note) async {
+  addNote(NoteModel note) {
     emit(AddNoteLoading());
     try {
       Box<NoteModel> notesBox = Hive.box<NoteModel>(kbox);
-      await notesBox.add(note);
+      notesBox.add(note);
+      print(
+          'notes Box : ${notesBox.values.first}\n notesBox : ${notesBox.isEmpty} ');
       emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNoteFaiulre(errorMessage: e.toString()));
